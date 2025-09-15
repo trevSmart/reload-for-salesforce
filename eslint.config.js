196e1f2abd198ffdcbf6da23ea91bd20acf82403
trevSmart <203040921+trevSmart@users.noreply.github.com>
2025-09-16 01:39:50 +0200

    Actualización de la flexipage Reload_Manager para incluir una nueva instancia de componente, reloadWorkbench, y optimización de la descripción. Esta modificación mejora la funcionalidad y la organización del espacio de trabajo para la carga de datos en Salesforce.

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,9 @@ const lwcPlugin = require("@lwc/eslint-plugin-lwc");
 const globals = require("globals");
 
 module.exports = defineConfig([
-  // LWC configuration
+  // Base configuration for JavaScript files
   {
-    files: ["**/lwc/**/*.js"],
-    plugins: {
-      "@lwc/lwc": lwcPlugin
-    },
-    rules: {
-      "@lwc/lwc/no-api-reassignments": "error",
-      "@lwc/lwc/no-async-await": "error",
-      "@lwc/lwc/no-async-operation": "error",
-      "@lwc/lwc/no-attributes-during-construction": "error",
-      "@lwc/lwc/no-document-query": "error",
-      "@lwc/lwc/no-dupe-class-members": "error",
-      "@lwc/lwc/no-for-of": "error",
-      "@lwc/lwc/no-inner-html": "error",
-      "@lwc/lwc/no-leaky-event-listeners": "error",
-      "@lwc/lwc/no-template-children": "error",
-      "@lwc/lwc/prefer-custom-event": "error",
-      "@lwc/lwc/valid-api": "error",
-      "@lwc/lwc/valid-track": "error",
-      "@lwc/lwc/valid-wire": "error"
-    },
+    files: ["**/*.js"],
     languageOptions: {
       parser: require("@babel/eslint-parser"),
       parserOptions: {
@@ -46,6 +27,36 @@ module.exports = defineConfig([
         ...globals.browser,
         ...globals.es2021
       }
+    },
+    rules: {
+      ...eslintJs.configs.recommended.rules
     }
+  },
+  // LWC specific configuration
+  {
+    files: ["**/lwc/**/*.js"],
+    plugins: {
+      "@lwc/lwc": lwcPlugin
+    },
+    rules: {
+      "@lwc/lwc/no-api-reassignments": "error",
+      "@lwc/lwc/no-async-await": "error",
+      "@lwc/lwc/no-async-operation": "error",
+      "@lwc/lwc/no-attributes-during-construction": "error",
+      "@lwc/lwc/no-document-query": "error",
+      "@lwc/lwc/no-dupe-class-members": "error",
+      "@lwc/lwc/no-for-of": "error",
+      "@lwc/lwc/no-inner-html": "error",
+      "@lwc/lwc/no-leaky-event-listeners": "error",
+      "@lwc/lwc/no-template-children": "error",
+      "@lwc/lwc/prefer-custom-event": "error",
+      "@lwc/lwc/valid-api": "error",
+      "@lwc/lwc/valid-track": "error",
+      "@lwc/lwc/valid-wire": "error"
+    }
+  },
+  // Ignore Apex files since ESLint doesn't have native Apex support
+  {
+    ignores: ["**/*.cls", "**/*.trigger"]
   }
 ]);
